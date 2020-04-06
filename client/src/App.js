b92e77220914ba77685512fd6dd8a32810605d2b
DEVfancybear <duonghacker2345@gmail.com>
2020-04-06 21:39:11 +0700

    react router dom client

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Fragment} from 'react';
+import {
+    Switch,
+    Route,
+} from "react-router-dom";
 import './App.css';
+import Home from "./components/Home";
+import About from "./components/About";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <Fragment className="App">
+            <Switch>
+                <Route exact path="/" component={Home}/>
+                <Route path="/about" component={About}/>
+            </Switch>
+        </Fragment>
+    );
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -6,16 +6,20 @@ import * as serviceWorker from './serviceWorker';
 import rootReducers from "./reducers/index";
 import {createStore, applyMiddleware} from "redux";
 import thunk from "redux-thunk";
-import { composeWithDevTools } from 'redux-devtools-extension';
-import {Provider} from "react-redux"
+import {composeWithDevTools} from 'redux-devtools-extension';
+import {Provider} from "react-redux";
+import {BrowserRouter} from "react-router-dom";
+
 const store = createStore(
     rootReducers, composeWithDevTools(applyMiddleware(thunk))
 )
 ReactDOM.render(
-<Provider store={store}>
-    <App />
+    <Provider store={store}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
     </Provider>,
-document.getElementById("root")
+    document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
