62ffe3ae1d72641d1e2d4b2f61466eed4f4c1aaf
DEVfancybear <duonghacker2345@gmail.com>
2020-04-06 21:19:01 +0700

    config npm run dev client vs server

--- index.js
+++ index.js
@@ -40,18 +40,14 @@ app.use('/api/users', require('./routes/users'));
 //https://stackoverflow.com/questions/48914987/send-image-path-from-node-js-express-server-to-react-client
 app.use('/uploads', express.static('uploads'));
 
-// Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
-
-    // Set static folder
-    // All the javascript and css files will be read and served from this folder
-    app.use(express.static("client/build"));
-
-    // index.html for all page routes    html or routing and naviagtion
+    app.use(express.static(path.join(__dirname, "client/build")));
+    //
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+        res.sendfile(path.join((__dirname = "client/build/index.html")));
     });
 }
+
 
 const port = process.env.PORT || 5000
 
