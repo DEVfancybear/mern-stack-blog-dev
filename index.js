dfe0a5b09341850544993d14db6e7b9107f91a07
DEVfancybear <duonghacker2345@gmail.com>
2020-04-06 14:42:06 +0700

    server connect database mongoose

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://duong113:123@cluster0-wn0ns.mongodb.net/test?retryWrites=true&w=majority',
+    {useNewUrlParser: true}).then(() => console.log('Connected success Mongoose !')).catch(err => {
+    console.log(err);
+})
+const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
