f84e9a7e2be834352085a6d318849af49de6d824
DEVfancybear <duonghacker2345@gmail.com>
2020-04-06 15:35:29 +0700

    add functionality register and test using postman

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 const express = require('express');
-const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const {User} = require('./models/user');
+const port = 5000;
+// connect database mongoose
 mongoose.connect('mongodb+srv://duong113:123@cluster0-wn0ns.mongodb.net/test?retryWrites=true&w=majority',
     {useNewUrlParser: true}).then(() => console.log('Connected success Mongoose !')).catch(err => {
     console.log(err);
-})
-const port = 3000;
-
+});
+const app = express();
+//to not get any deprecation warning or error
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
+//to get json data
+// support parsing of application/json type post data
+app.use(bodyParser.json());
+app.use(cookieParser());
+// unlock cors
+app.use(cors());
+// method
 app.get('/', (req, res) => res.send('Hello World!'));
+app.post('/api/users/register', (req, res) => {
+    const user = new User(req.body);
+    user.save((err, userData) => {
+        if (err) return res.json({success: false, err});
+        return res.status(200).json({
+            success: true
+        });
+    });
+});
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
