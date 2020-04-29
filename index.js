c742cb98968b7e4a55524ee5e505795a117ac4c9
DEVfancybear <duonghacker2345@gmail.com>
2020-04-29 11:00:34 +0700

    User registration

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const connectDB = require("./config/db");
-const bodyParser = require("body-parser")
 const app = express();
 //Connect database
 connectDB();
 app.get("/", (req, res) => res.send("Welcome to database"));
 // Init Middleware
-app.use(express.json({extended: false}))
+app.use(express.json({ extended: false }));
 //Define Routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/posts", require("./routes/api/posts"));
 app.use("/api/profile", require("./routes/api/profile"));
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
 
