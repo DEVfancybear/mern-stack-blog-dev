e9b39da699ee09dbac5dc884f5bb469be6a6856f
DEVfancybear <duonghacker2345@gmail.com>
2020-04-30 22:34:40 +0700

    create component Experience

---
+++ client/src/components/dashboard/Experience.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {connect} from "react-redux";
+const Experience = () => {
+    return (
+        <div>
+            Experience
+        </div>
+    )
+}
+export default connect(null,null)(Experience);
