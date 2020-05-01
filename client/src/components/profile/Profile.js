b2d55e310e838903f340a26380d23d1ad1e2443c
DEVfancybear <duonghacker2345@gmail.com>
2020-05-01 08:13:35 +0700

    create component ProfileEducation

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -6,6 +6,7 @@ import {Link} from "react-router-dom";
 import ProfileTop from "./ProfileTop";
 import ProfileAbout from "./ProfileAbout";
 import ProfileExperience from "./ProfileExperience";
+import ProfileEducation from "./ProfileEducation";
 
 const Profile = ({getProfileById, profileReducers: {profile, loading}, authReducers, match}) => {
     useEffect(() => {
@@ -34,6 +35,21 @@ const Profile = ({getProfileById, profileReducers: {profile, loading}, authReduc
                                 </Fragment>
                             ) : (
                                 <h4>No experience credentials</h4>
+                            )}
+                        </div>
+                        <div className="profile-edu bg-white p-2">
+                            <h2 className="text-primary">Education</h2>
+                            {profile.education.length > 0 ? (
+                                <Fragment>
+                                    {profile.education.map((education) => (
+                                        <ProfileEducation
+                                            key={education._id}
+                                            education={education}
+                                        />
+                                    ))}
+                                </Fragment>
+                            ) : (
+                                <h4>No education credentials</h4>
                             )}
                         </div>
                     </div>
